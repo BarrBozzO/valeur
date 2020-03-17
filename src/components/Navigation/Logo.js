df3e48ddd7de0787a72554dbd230a1ac10dfaeab
Constantine <barr.develop@gmail.com>
2020-03-17 10:02:43 +0400

    add logo link to home

--- src/components/Navigation/Logo.js
+++ src/components/Navigation/Logo.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Link } from "gatsby";
 
 import styles from "./Navigation.module.scss";
 
 function Logo() {
-  return <div className={styles["navigation__logo"]}>logo</div>;
+  return (
+    <div className={styles["navigation__logo"]}>
+      <Link to="/" />
+    </div>
+  );
 }
 
 export default Logo;
